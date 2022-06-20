eb820eb5d81306daf507a36cbef23e40900c62f5
ADMIN <ADMIN@DESKTOP-DKDQVD8>
2022-06-20 17:21:16 +0530

    i shared the project

--- src/main/java/GroupIdmavenproject/ArtifactIdmavenproject/App.java
+++ src/main/java/GroupIdmavenproject/ArtifactIdmavenproject/App.java
@@ -2,6 +2,6 @@ package GroupIdmavenproject.ArtifactIdmavenproject;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.out.println("kiran has explained the git hub");
   }
 }
