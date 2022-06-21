ea2510d977ebc35d2872968be73ee5873e627df5
HP <HP@LAPTOP-L2KAM24D>
2022-06-21 11:45:26 +0530

    i updated the program int a =20+30;

--- src/main/java/GroupIdmavenproject/ArtifactIdmavenproject/App.java
+++ src/main/java/GroupIdmavenproject/ArtifactIdmavenproject/App.java
@@ -2,6 +2,8 @@ package GroupIdmavenproject.ArtifactIdmavenproject;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("kiran has explained the git hub");
+	  
+	  int a= 20+30;
+System.out.println(a);
   }
 }
