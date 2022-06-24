40c3b2008eefb8db91a192804bc955e6a6392dbf
saikumarneredi <107617072+saikumarneredi@users.noreply.github.com>
2022-06-24 09:17:25 -0700

    Update App.java

--- src/main/java/GroupIdmavenproject/ArtifactIdmavenproject/App.java
+++ src/main/java/GroupIdmavenproject/ArtifactIdmavenproject/App.java
@@ -3,5 +3,6 @@ package GroupIdmavenproject.ArtifactIdmavenproject;
 public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    System.out.println( "automation testing")
   }
 }
